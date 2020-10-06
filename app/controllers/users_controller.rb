66961b2f52fa251081fa0844a14c850cf05a6f67
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2020-10-06 19:51:56 +0900

    error css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,25 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  
+  # POST /users (+ params)
   def create 
     # user + (given params).save
+    # User.create(params[:user][:admin]) 想定していないparamsが混じってしまう可能性もあり
+    @user = User.new(user_params) 
+    if @user.save
+    else
+      render 'new'
+    end
   end
 
   def show
     @user = User.find(params[:id])
   end
-  
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+        :password_confirmation)
+    end
 end
