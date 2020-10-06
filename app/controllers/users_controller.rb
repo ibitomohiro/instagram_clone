166374b20698c048982c36b145389a9284903321
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2020-10-06 19:02:49 +0900

    user new view erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
+  #GET /users/new
   def new
+    @user = User.new
   end
+
+  
+  def create 
+    # user + (given params).save
+  end
+
   def show
     @user = User.find(params[:id])
   end
+  
 end
