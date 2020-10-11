5b998774b9124dfe94e8d60b5377288cbbab7787
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2020-10-11 12:38:10 +0900

    edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit 
+    @user = User.find(params[:id])
+  end
+
     private
 
     def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show, :edit]
   resources :sessions, only: [:new, :create, :destroy]
 end
