043e15b72fd328500bb222e5f36980477f9a6d03
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2020-10-11 21:41:43 +0900

    upload user image

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  mount_uploader :image, ImageUploader
   before_validation { email.downcase! }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
