1c5878500528b5c7af159ce63cddcc2b4d56fbc1
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2020-10-15 23:01:15 +0900

    validation image error object

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,5 +3,6 @@ class Picture < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence:true
+  validates :image, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
