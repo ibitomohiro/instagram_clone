b8cbdf45e30caae7cd4935eca80ba2cc7379495a
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2020-10-19 21:37:59 +0900

    お気に入り機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   resources :users, only: [:new, :create, :show, :edit, :update, :index]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :favorites, only:[:create, :destroy]
   resources :pictures do
     collection do
       post :confirm
