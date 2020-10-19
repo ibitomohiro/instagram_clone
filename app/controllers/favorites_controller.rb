de11ac228b2956297f7f3fe7cac51d8e7df0c574
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2020-10-19 23:04:47 +0900

    favorite index

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,20 @@
 class FavoritesController < ApplicationController
+
+  def index
+    @favorites = Favorite.all 
+  end
+
+  def new
+    @favorite = Favorite.new
+  end
+
   def create
-    favorite = current_user.favorites.create(picture_id: params[:picture_id])
+    @favorite = current_user.favorites.create(picture_id: params[:picture_id])
     redirect_to pictures_url, notice: "#{favorite.picture.user.name}さんのお気に入り登録をしました"
   end
 
   def destroy
-    favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    @favorite = current_user.favorites.find_by(id: params[:id]).destroy
     redirect_to pictures_url, notice: "#{favorite.picture.user.name}さんのお気に入りを解除しました"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   resources :users, only: [:new, :create, :show, :edit, :update, :index]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :favorites, only:[:create, :destroy]
+  resources :favorites, only:[:create, :destroy, :index]
   resources :pictures do
     collection do
       post :confirm
