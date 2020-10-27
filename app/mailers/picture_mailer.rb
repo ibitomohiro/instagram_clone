5696afa395652aa6df8c15884343034b645b9e84
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2020-10-27 22:14:08 +0900

    changed email address

--- app/mailers/picture_mailer.rb
+++ app/mailers/picture_mailer.rb
@@ -1,6 +1,6 @@
 class PictureMailer < ApplicationMailer
   def picture_mail(picture)
     @picture = picture
-    mail to: @picture.user.email, subject:"Pictureの投稿の確認のメール"
+    mail to: "ibitomohiro@gmail.com", subject:"Pictureの投稿の確認のメール"
   end
 end
